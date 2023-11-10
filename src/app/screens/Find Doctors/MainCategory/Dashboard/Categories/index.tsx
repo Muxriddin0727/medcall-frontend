690b17737d9b2b5fb1b06d7b57390a133588c55c
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2023-11-10 23:27:28 +0900

    feat: getdoctorbycategory

--- src/app/screens/Find Doctors/MainCategory/Dashboard/Categories/index.tsx
+++ src/app/screens/Find Doctors/MainCategory/Dashboard/Categories/index.tsx
@@ -1,16 +1,34 @@
-import type { FC } from "react";
+import { useState, type FC, useEffect } from "react";
+import { useAxios } from "../../../../../customHooks/useAxios";
+import { useSearchParams } from "react-router-dom";
 
 const Categories: FC = () => {
+  const [params, setParams] = useSearchParams();
+  const axios = useAxios();
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios({
+      url: "/client/category",
+    }).then((res) => {
+      setData(res.data.data);
+    });
+  }, [] );
+
+  const active_category = params.get("category");
+
   return (
     <div>
       <h3 className="  font-bold">Categories</h3>
       <div>
-        {Array.from({ length: 10 }).map(( _, idn: number ) =>(
+        {data.map(( value: {name: string, count:number, route_path: string, _id: string},  ) =>(
         <div 
-        key={idn}
-        className="flex justify-between mt-[10px] pl-[16px] cursor-pointer hover:text-[#06B6D4] transition-colors">
-          <h3 className="text-base font-normal ">Cardiology</h3>
-          <p className="text-base font-normal ">(12)</p>
+        onClick={() => setParams({ category: value.route_path })}
+        key={value._id}
+        className={`flex justify-between mt-[10px] pl-[16px] cursor-pointer hover:text-[#06B6D4] transition-colors
+        ${active_category === value.route_path && "text-[#06B6D4]"} `}>
+          <h3 className="text-base font-normal ">{value.name }</h3>
+          <p className="text-base font-normal ">({value.count})</p>
         </div>
          ))}
       </div>
