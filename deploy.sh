b5b0bee90bbcd58764ba6419d08a2adcfab2c54c
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2023-12-24 17:17:45 +0900

    feat: error fix

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ git pull origin develop
 npm i yarn -g
 yarn
 yarn run build
-pm2 start yarn --interpreter bash --name=GuardianCare-React --start:prod
+pm2 start "yarn run start:prod" --name=GuardianCare-REACT
 
  # Development
 # npm i yarn -g
