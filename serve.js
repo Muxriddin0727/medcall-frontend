121e02816e9d2f97c2faae127f20ffd3bf8a0a12
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2023-12-25 23:11:02 +0900

    feat: fix11

--- serve.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process');
-exec('npx serve -s build', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.error(`stderr: ${stderr}`);
-});
