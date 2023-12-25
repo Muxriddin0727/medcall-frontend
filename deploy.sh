f57e4bff77a06a81f7be3813b8915fecebdb8323
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2023-12-26 04:44:46 +0900

    fix:fix

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ git pull origin develop
 npm i yarn -g
 yarn
 yarn run build
-pm2 start "C:\Users\abduv\Desktop\medcall-frontend\node_modules\serve\bin\serve.js" --name "Guardian_Care-React" -- -s build
+pm2 start "C:\Users\abduv\Desktop\medcall-frontend\node_modules\serve" --name "Guardian_Care-React" -- -s build
 
 
 
