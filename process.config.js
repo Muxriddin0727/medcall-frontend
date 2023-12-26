790ecb1cc090315f237cc6ae46e2782be425342c
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2023-12-26 20:26:35 +0900

    fix: fix21

--- process.config.js
+++ process.config.js
@@ -3,7 +3,6 @@ module.exports = {
       {
         name: 'Guardian_Care-React',
         script: 'serve',
-        interpreter: 'bash',
         args: '-s build',
         env: {
           NODE_ENV: 'production',
