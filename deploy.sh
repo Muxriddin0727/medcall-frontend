0a873db2b38a5ae01d176d39d7e0f9e557f856db
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2024-01-17 17:06:27 +0900

    added serve globally

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@
 # Production
 git reset --hard
 git pull origin develop
+
 npm i yarn -g
+yarn global add serve
 yarn
 yarn run build
 pm2 start yarn --name "Guardian_Care_React" --yarn run "start:prod"
