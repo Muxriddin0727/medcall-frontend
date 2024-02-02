969f57632c395278d497d7cc54201c5bcbd52249
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2024-02-03 01:10:17 +0900

    feat: authorizatin modified to refactor lgout

--- src/app/customHooks/useAxios/index.ts
+++ src/app/customHooks/useAxios/index.ts
@@ -3,52 +3,51 @@ import store from "../../redux";
 import { setTokenExpired } from "../../redux/userSlice";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://46.28.44.182:3002',
- });
- 
- // Add a request interceptor to attach the token to each request
- axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-     config.headers['Authorization'] = `Bearer ${token}`;
-  }
-  return config;
- }, (error) => {
-  return Promise.reject(error);
- });
- 
- // Add a response interceptor to handle token expiration
- axiosInstance.interceptors.response.use((response) => {
-  return response;
- }, (error) => {
-  if (error.response && error.response.status === 401) {
-     store.dispatch(setTokenExpired()); // Dispatch the Redux action to handle token expiration
-  }
-  return Promise.reject(error);
- });
+ baseURL: 'http://46.28.44.182:3002',
+});
+
+// Add a request interceptor to attach the token to each request
+axiosInstance.interceptors.request.use((config) => {
+ const token = localStorage.getItem('token');
+ if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+ }
+ return config;
+}, (error) => {
+ return Promise.reject(error);
+});
+
+// Add a response interceptor to handle token expiration
+axiosInstance.interceptors.response.use((response) => {
+ return response;
+}, (error) => {
+ if (error.response && error.response.status === 401) {
+    store.dispatch(setTokenExpired()); // Dispatch the Redux action to handle token expiration
+ }
+ return Promise.reject(error);
+});
 
 export const useAxios = () => {
-  const request = async ({
+ const request = async ({
     url,
     method = "GET",
     body,
     headers,
-  }: {
+ }: {
     url: string;
     method?: "GET" | "POST" | "PUT" | "DELETE";
     body?: object;
     headers?: object;
-  }) => {
+ }) => {
     return axiosInstance({
-      url: `http://46.28.44.182:3002${url}`,
+      url,
       method,
       data: body,
       headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
         ...headers,
       },
     });
-  };
+ };
 
-  return request;
+ return request;
 };
