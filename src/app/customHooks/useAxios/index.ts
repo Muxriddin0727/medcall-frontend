ded087667f41644a3cc24a3cb46f383ca9f8904f
Muxriddin0727 <abduvohidov.muxriddin0727@gmail.com>
2024-02-03 01:02:44 +0900

    fix: logout modified to work when token expires

--- src/app/customHooks/useAxios/index.ts
+++ src/app/customHooks/useAxios/index.ts
@@ -1,4 +1,31 @@
 import axios from "axios";
+import store from "../../redux";
+import { setTokenExpired } from "../../redux/userSlice";
+
+const axiosInstance = axios.create({
+  baseURL: 'http://46.28.44.182:3002',
+ });
+ 
+ // Add a request interceptor to attach the token to each request
+ axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+     config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+ }, (error) => {
+  return Promise.reject(error);
+ });
+ 
+ // Add a response interceptor to handle token expiration
+ axiosInstance.interceptors.response.use((response) => {
+  return response;
+ }, (error) => {
+  if (error.response && error.response.status === 401) {
+     store.dispatch(setTokenExpired()); // Dispatch the Redux action to handle token expiration
+  }
+  return Promise.reject(error);
+ });
 
 export const useAxios = () => {
   const request = async ({
@@ -12,7 +39,7 @@ export const useAxios = () => {
     body?: object;
     headers?: object;
   }) => {
-    return axios({
+    return axiosInstance({
       url: `http://46.28.44.182:3002${url}`,
       method,
       data: body,
